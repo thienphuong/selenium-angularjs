606849d8b31cfe33213ce58ea23f7ff0196643aa
Jacob Vu <jacob.vu@linecorp.com>
2019-06-27 21:10:48 +0700

    Init project

---
+++ src/test/java/com/todomvc/features/web/GoogleTest.java
@@ -0,0 +1,14 @@
+package com.todomvc.features.web;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class GoogleTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
